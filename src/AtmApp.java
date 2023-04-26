1c7b0259c67a48f17339055b74d0d065528da3ae
Leyla Simitci <leylacayli@hotmail.com>
2023-04-26 12:02:20 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -3,5 +3,7 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("bu bir ATM projesidir");
         //version1
+        System.out.println("java is easy");
+        //version2
     }
 }
